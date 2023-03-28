f531639684283ad69f0fc1101fae38716be5f416
jeevansai76 <127392270+jeevansai76@users.noreply.github.com>
2023-03-28 22:25:28 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+function add(x,y){
+    return x+y;
+};
+
+function sub(x,y){
+    return x-y;
+};
+
+function mul(x,y){
+    return x*y;
+};
+
+function div(x,y){
+    return x/y;
+};
+
+experts.add=add;
+experts.sub=sub;
+experts.mul=mul;
+experts.div=div;
